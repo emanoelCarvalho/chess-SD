3f3277bf071568b5f4b4d29232d0882e966727f4
emanoelCarvalho <ehcs@discente.ifpe.edu.br>
2025-02-27 10:33:34 -0300

    feat: add PlayersModule and GameModule to app.module.ts

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GameGateway } from './game/game.gateway';
+import { PlayersModule } from './players/players.module';
+import { GameModule } from './game/game.module';
 
 @Module({
-  imports: [],
+  imports: [PlayersModule, GameModule],
   controllers: [AppController],
   providers: [AppService, GameGateway],
 })
