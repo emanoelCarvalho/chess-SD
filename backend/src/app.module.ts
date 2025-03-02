994f7ee050fa74c4f9cd4bbd19fb3f354c608109
emanoelCarvalho <ehcs@discente.ifpe.edu.br>
2025-03-01 22:01:34 -0300

    feat: integrate ConfigModule for global configuration management

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,17 @@ import { GameGateway } from './game/game.gateway';
 import { PlayersModule } from './players/players.module';
 import { GameModule } from './game/game.module';
 import { DatabaseModule } from './database/database.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [PlayersModule, GameModule, DatabaseModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    PlayersModule,
+    GameModule,
+    DatabaseModule,
+  ],
   providers: [GameGateway],
 })
 export class AppModule {}
