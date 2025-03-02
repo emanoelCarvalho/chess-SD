af4ef6671971e636c516641319f955a66b23caf9
emanoelCarvalho <ehcs@discente.ifpe.edu.br>
2025-03-01 21:54:10 -0300

    feat: add DatabaseModule and integrate with AppModule

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { GameGateway } from './game/game.gateway';
 import { PlayersModule } from './players/players.module';
 import { GameModule } from './game/game.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [PlayersModule, GameModule],
+  imports: [PlayersModule, GameModule, DatabaseModule],
   providers: [GameGateway],
 })
 export class AppModule {}
